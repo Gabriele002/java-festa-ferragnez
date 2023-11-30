e6ad7efc3dd155033ac06a70c94d953254f13411
Gabriele002 <gabrieleciarleglio97@gmail.com>
2023-11-30 14:02:52 +0100

    add array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,14 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        String [] invited = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax",
+                "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei",
+                "Martina Maccherone", "Rachel Zeilic"};
     }
+
 }
