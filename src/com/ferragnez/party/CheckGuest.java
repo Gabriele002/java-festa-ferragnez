a6f72e23e840beb518e09a550882010cfdd954d3
Gabriele002 <gabrieleciarleglio97@gmail.com>
2023-11-30 15:45:57 +0100

    finish work

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -8,7 +8,26 @@ public class CheckGuest {
 
         String [] invited = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax",
                 "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei",
-                "Martina Maccherone", "Rachel Zeilic"};
-    }
+                "Martina Maccherone", "Rachel Zeilic","Matteo Musella"};
 
+        System.out.print("Whats your name? ");
+        String nameInvited = scan.nextLine();
+        boolean invitato = false;
+
+        for (int i = 0; i < invited.length && !invitato ; i++) {
+            if (nameInvited.equals(invited[i])) {
+                invitato = true;
+
+            }
+
+            }
+            if (invitato) {
+                System.out.println("Presente nella lista");
+            } else {
+                System.out.println("Mi dispiace non sei stato invitato");
+            }
+
+        scan.close();
+
+    }
 }
